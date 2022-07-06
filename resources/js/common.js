c8b578ffff1bcfdf8288ef936172ffb4a279321b
Yassen Shopov <yassenshopov00@gmail.com>
2022-07-06 12:49:48 +0300

    Fixed responsiveness of Mobile Menu

--- resources/js/common.js
+++ resources/js/common.js
@@ -2,14 +2,19 @@ let click = true;
 
 function animate_menu(x) {
     click = !click;
-    console.log(click)
     if (click == false) {
         document.getElementById("overlay").style.height = "100vh";
         document.body.style.overflowY = "hidden";
+        document.getElementById("bar1").classList.add("onClick1");
+        document.getElementById("bar2").classList.add("onClick2");
+        document.getElementById("bar3").classList.add("onClick3");
         // document.getElementById("overlay").style.display = "block";
     } else {
         document.getElementById("overlay").style.height = "0";
         document.body.style.overflowY = "visible";
+        document.getElementById("bar1").classList.remove("onClick1");
+        document.getElementById("bar2").classList.remove("onClick2");
+        document.getElementById("bar3").classList.remove("onClick3");
         // document.getElementById("overlay").style.display = "none";
     }
 }
